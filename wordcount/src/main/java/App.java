ee55cdcb92a092f06b290634c4fddae923a71df5
Aleksey Titov <admin>
2019-03-31 17:16:05 -0400

    Update

--- wordcount/src/main/java/App.java
+++ wordcount/src/main/java/App.java
@@ -1,63 +1,34 @@
-import tech.tablesaw.api.DoubleColumn;
-import tech.tablesaw.api.IntColumn;
-import tech.tablesaw.api.Table;
-import tech.tablesaw.columns.numbers.IntColumnType;
-import tech.tablesaw.plotly.Plot;
-import tech.tablesaw.plotly.api.HorizontalBarPlot;
-import tech.tablesaw.plotly.api.PiePlot;
-import tech.tablesaw.plotly.components.Figure;
-import tech.tablesaw.plotly.components.Layout;
-import tech.tablesaw.plotly.traces.BarTrace;
+import com.kennycason.kumo.CollisionMode;
+import com.kennycason.kumo.WordCloud;
+import com.kennycason.kumo.WordFrequency;
+import com.kennycason.kumo.bg.PixelBoundryBackground;
+import com.kennycason.kumo.font.scale.LinearFontScalar;
+import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+import com.kennycason.kumo.palette.ColorPalette;
+
+import java.awt.*;
+import java.io.FileNotFoundException;
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
-
-        //***************** Setup *************************
-        // load the data into a table
-        Table tornadoes = Table.read().csv("../data/tornadoes_1950-2014.csv");
-
-        // Get the scale column and replace any values of -9 with the column's missing value indicator
-        IntColumn scale = tornadoes.intColumn("scale");
-        scale.set(scale.isEqualTo(-9), IntColumnType.missingValueIndicator());
-
-        //***************** Plotting **********************
-
-        // BAR PLOTS
-
-        // Sum the number of fatalities from each tornado, grouping by scale
-        Table fatalities1 =
-                tornadoes.summarize("fatalities", sum).by("scale");
-
-        // Plot
-        Plot.show(
-                HorizontalBarPlot.create(
-                        "fatalities by scale",		        // plot title
-                        fatalities1,				            // table
-                        "scale",					// grouping column name
-                        "sum [fatalities]"));		// numeric column name
-
-        // Plot the mean injuries rather than a sum.
-        Table injuries1 = tornadoes.summarize("injuries", mean).by("scale");
-
-        Plot.show(
-                HorizontalBarPlot.create("Average number of tornado injuries by scale",
-                        injuries1, "scale", "mean [injuries]"));
+    public static void main( String[] args ) throws FileNotFoundException, java.io.IOException  {
+        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+        frequencyAnalyzer.setWordFrequenciesToReturn(300);
+        frequencyAnalyzer.setMinWordLength(2);
 
 
-        // PIE PLOT
-        Plot.show(
-                PiePlot.create("fatalities by scale", fatalities1, "scale", "sum [fatalities]"));
+        final java.util.List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("song.txt");
 
-        // PARETO PLOT
-        Table t2 = tornadoes.summarize("fatalities", sum).by("State");
+        final Dimension dimension = new Dimension(600, 300);
+        final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+        wordCloud.setPadding(2);
+        wordCloud.setBackground(new PixelBoundryBackground("queen.png"));
+        wordCloud.setFontScalar(new LinearFontScalar(4, 10));
+        wordCloud.setBackgroundColor(new Color(0xFFFFFF));
+        wordCloud.setColorPalette(new ColorPalette(new Color(0xAA0000), new Color(0xFFFFFF)));
 
-        t2 = t2.sortDescendingOn(t2.column(1).name());
-        Layout layout = Layout.builder().title("Tornado Fatalities by State").build();
-        BarTrace trace = BarTrace.builder(
-                t2.categoricalColumn(0),
-                t2.numberColumn(1))
-                .build();
-        Plot.show(new Figure(layout, trace));
+
+        wordCloud.build(wordFrequencies);
+        wordCloud.writeToFile("wordcloud.png");
     }
 }
--- wordcount/src/main/java/WordCount.java
+++ wordcount/src/main/java/WordCount.java
@@ -5,41 +5,42 @@ import java.util.*;
 
 public class WordCount {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        try {
-            Scanner scanner = new Scanner(new File("song.txt"));
-            HashMap<String, Integer> lyricCount = new HashMap<>();
+        Scanner scanner = new Scanner(new File("song.txt"));
+        HashMap<String, Integer> lyricCount = new HashMap<>();
 
-            while (scanner.hasNext()) {
-                String word = scanner.next();
-                String strip = word.replaceAll("\\p{Punct}", "").toLowerCase();
-                int count = lyricCount.getOrDefault(strip, 0) + 1;
-                lyricCount.put(strip, count);
-            }
-
-            HashMap<String, Integer> sortedLyrics = sortByValue(lyricCount);
-            PrintWriter print = new PrintWriter("output.txt");
-
-            print.println("Total word count: " + sortedLyrics.size());
-            print.println("-------------------------------");
-            for (String word : sortedLyrics.keySet()) {
-                print.printf("Word: %-10s Count: %s\n", word,  sortedLyrics.get(word));
-            }
-            print.flush();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            String strip = word.replaceAll("\\p{Punct}", "").toLowerCase();
+            int count = lyricCount.getOrDefault(strip, 0) + 1;
+            lyricCount.put(strip, count);
         }
+
+        HashMap<String, Integer> sortedLyrics = sortByValue(lyricCount);
+        PrintWriter print = new PrintWriter("output.txt");
+
+        print.println("Total word count: " + sortedLyrics.size());
+        print.println("-------------------------------");
+
+        // Printing all words with count.
+        for (String word : sortedLyrics.keySet()) {
+            print.printf("Word: %-10s Count: %s\n", word, sortedLyrics.get(word));
+        }
+
+        // Flushing data and closing all connections used.
+        print.flush();
+        print.close();
+        scanner.close();
     }
 
     private static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm) {
 
         // Create a list from elements of HashMap
-        List<Map.Entry<String, Integer> > list = new LinkedList<>(hm.entrySet());
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(hm.entrySet());
 
         // Sort the list
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
             public int compare(Map.Entry<String, Integer> o1,
                                Map.Entry<String, Integer> o2) {
                 return (o2.getValue()).compareTo(o1.getValue());
